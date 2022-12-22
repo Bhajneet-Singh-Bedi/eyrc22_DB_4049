afe17f3999b03efa51d92d168f012b325ce3ebb9
Bhajneet-Singh-Bedi <bhajneetbedi2002@gmail.com>
2022-12-23 01:42:33 +0530

    New working design, reached bumper-2 and S curve.

--- task_3A/test/RW_Pendulum.m
+++ task_3A/test/RW_Pendulum.m
@@ -238,12 +238,13 @@ function RW_pendulum_main()
   #m1 = 8;     # Mass of Pendulum Bar
   #m1 = 2;
   #m1 = 13;
-  m1 = 14.18;
+  m1=15.9245;
   #m2= 0.08;   # Mass of Reaction Wheel 
   m2 = 4.952;
   #l1= 3;      # Length of Pendulum Bar
   #l1 = 0.8;
-  l1 = 0.6;
+  #l1 = 0.6;
+  l1 = 0.7;
   g = 9.8;    # Centre of Gravity
   y0 = [0.9*pi; 0; 0; 0]; # Initial Conditions
   y_setpoint = [pi; 0; 2*pi; 0];  # Reference Point
