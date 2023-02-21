f3135a90f2946357c686e92168128847dfda748d
Bhajneet-Singh-Bedi <bhajneetbedi2002@gmail.com>
2023-02-21 23:43:11 +0530

    Added task5_A and zip

---
+++ task_5A/zips/4049_Task5A/newBalancing.ino
@@ -0,0 +1,189 @@
+#include <util/atomic.h>
+#include <MPU6050_light.h>
+
+#define ENCA 2 //21-PD0 //19-RX1  //17-RX2
+#define ENCB 3 //20-PD1 //18-TX1  //16-TX2
+#define brake 48 //PL1
+#define cw 11 // 1A
+#define pwm 9 // 
+
+MPU6050 mpu(Wire);
+
+// globals
+float prev = 0, K[4]={}, y[4]={}, y_setpoint[4]={}, M;
+float thet, thet_dot, alp, alp_dot, prevAlp=0, vGive, prev_u=0;
+long prevT=0;
+int posPrev=0,  sz=4, trq;
+volatile int pos_i=0; 
+volatile float velocity_i=0;
+volatile long prevT_i=0;
+
+
+void readEncoder(){
+  int b=digitalRead(ENCB);
+  int increment=0;
+  if (b>0){
+    increment=1;
+//    pos_i++;
+  }
+  else{
+    increment=-1;
+//    pos_i--;
+  }
+  pos_i=pos_i+increment;
+//  Serial.println(pos_i);
+
+  long currT=micros();
+  float deltaT=((float) (currT-prevT_i))/1.0e6;
+  velocity_i=increment/deltaT;
+  prevT_i=currT;
+}
+
+
+void setMotor(int dir, int pwr, float prev_u){
+  
+  if (dir == 1){
+    if (prev_u == -1){
+      digitalWrite(brake, LOW);
+    }
+    digitalWrite(cw, LOW);
+  }
+  else{
+    if (prev_u == 1){
+      digitalWrite(brake, LOW);
+    }
+    digitalWrite(cw, HIGH);
+  }
+//  Serial.println(255-pwr);
+  digitalWrite(brake, HIGH);
+  analogWrite(pwm, 255-pwr);
+}
+
+int lqrController(float yy[], float yy_setpoint[]){
+  float mul=0;
+  //  K = [-311.4268,-85.9813,-1.0000,-1.7637];
+//  K[0]=-41.4663;K[1]=-6.4185;K[2]=-1.0000;K[3]=-1.3120;
+//  K[0]=-49.32124;K[1]=-7.25900;K[2]=-1.00000;K[3]=-1.29629;
+//  K[0]=-49.91982;K[1]=-7.31929;K[2]=-1.00000;K[3]=-1.29515;
+//  K[0]=-69.90391;K[1]=-10.38223;K[2]=-1.00000;K[3]=-1.29991;
+//  K[0]=-69.97770;K[1]=-10.38361;K[2]=-1.00000;K[3]=-1.30019;
+//  K[0]=-69.89653;K[1]=-10.38210;K[2]=-1.00000;K[3]=-1.2998;4
+//  K[0]=-77.60897;K[1]=-10.52474;K[2]=-1.00000;K[3]=-1.32904;
+//  K[0] = -22.78308;K[1]=-3.37165;K[2]=-0.31623;K[3]=-0.41236;
+//  K[0] = -49.71888;K[1]=-7.37913;K[2]=-0.70711;K[3]=-0.91972;
+//    K[0]=-69.90391;  K[1]=-10.38223;   K[2]=-1.00000;   K[3]=-1.29991;
+//    K[0]=-40.77681;   K[1]=-6.04874;   K[2]=-0.57735;   K[3]=-0.75130;
+//    K[0]=-193.01827;   K[1]=-25.14391;    K[2]=-1.00000;    K[3]=-1.26103;
+//    K[0]=-136.66472;   K[1]=-17.80188;    K[2]=-0.70711;    K[3]=-0.89180;
+//    K[0]=-281.02199;   K[1]=-35.01541;    K[2]=-0.70711;    K[3]=-0.88350;
+//    K[0]=-217.99154;   K[1]=-27.49369;    K[2]=-0.70711;    K[3]=-0.88573;
+//    K[0]=-147.40983;   K[1]=-18.80253;    K[2]=-0.57735;    K[3]=-0.72496;
+//    K[0]=-147.40983;   K[1]=-18.80253;    K[2]=-0.70711;    K[3]=-0.72496;
+//    K[0]=-26.01250;   K[1]=-3.31595;   K[2]=-0.10000;   K[3]=-0.12580;
+//    K[0]=-28.07205;   K[1]=-3.65106;   K[2]=-0.14142;   K[3]=-0.17865;
+    K[0]=-28.47513;   K[1]=-3.65753;   K[2]=-0.14142;   K[3]=-0.17913;
+  for (int i=0; i<sz; i++){
+    mul=mul-(K[i]*(yy[i]-yy_setpoint[i]));
+  }
+  return mul;
+}
+
+
+void setTorque(float dtt, int trq, float vNow){
+  // T = M*angular acc.
+  vGive = ((trq*dtt)/M) + vNow;
+
+  Serial.println(vGive);
+  float kp=1;
+  float e=vGive-vNow;
+  float u=kp*e;
+
+  // Direction.
+  int dir=1;
+  if (u<0){
+    dir=-1;
+  }
+
+  int pwr = (int) fabs(u);
+
+  if (pwr>255){
+    pwr = 255;
+  }
+  setMotor(dir, pwr, prev_u);
+  prev_u=u;
+}
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Wire.begin();
+  mpu.begin();
+  pinMode(ENCA, INPUT);
+  pinMode(ENCB, INPUT);
+  pinMode(brake, OUTPUT);
+  pinMode(cw, OUTPUT);
+  pinMode(pwm, OUTPUT);
+//  digitalWrite(ENCA, HIGH);
+  digitalWrite(brake, LOW); // Low means braking.
+//  digitalWrite(cw, HIGH); // gives positive value // Low will give negative
+  analogWrite(pwm, 255); // 255 means stop // 0 means go.
+//  mpu.calcOffsets(true, true); 
+  M = 0.084*0.097*0.097/2; // Moment of Inertia.
+  attachInterrupt(digitalPinToInterrupt(ENCA), readEncoder, RISING);
+}
+
+void loop() {
+//  Serial.println(pos_i);
+
+
+  mpu.update();
+  thet = mpu.getAngleY()*0.0174533; // radians
+  // Theta_dot.
+  thet_dot = mpu.getGyroY()*0.0174533; // radians
+
+  float curr = micros();
+  alp = (pos_i*360*0.10471975512)/100; // radians
+  float dtt = curr - prev;
+  prev = curr;
+  alp_dot = (alp-prevAlp)/dtt;
+  prevAlp=alp;
+  
+  
+//  int pwr = 100/3.0*micros()/1.0e6;
+//  int dir = 1;
+//  setMotor(dir, pwr);
+//  Serial.print(thet);
+//  Serial.print("\t");
+//  Serial.print(thet_dot);
+//  Serial.println("\t");
+  
+  y[0]=thet; y[1]=thet_dot; y[2]=alp;  y[3]=alp_dot;
+  y_setpoint[0]=0;y_setpoint[1]=0;y_setpoint[2]=0;y_setpoint[3]=0;
+  trq = lqrController(y, y_setpoint);
+//  Serial.println(trq);
+  int pos=0;
+  float velocity2=0;
+  ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
+    pos = pos_i;
+    velocity2=velocity_i;
+  }
+
+//   Computing velocity with method 1
+//  long currT = micros();
+//  float deltaT = ((float) (currT-prevT))/1.0e6;
+//  float velocity1 = (pos-posPrev)/deltaT;
+//  posPrev=pos;
+//  prevT = currT;  
+
+
+  float v2 = velocity2/600.0*60.0;
+//  Serial.print(velocity1);
+//  Serial.print(" ");
+//  Serial.print(v2);
+//  Serial.println();
+  setTorque(dtt, trq, v2);
+
+
+}
